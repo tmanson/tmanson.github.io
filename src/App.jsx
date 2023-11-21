b57a5b21121621438dd8c303428131e6f11bad91
Tomy MANSON <mansoninformatique@gmail.com>
2023-11-21 23:03:32 +0100

    Init header

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,40 @@ import Header from "./components/Header.jsx";
 import Experiences from "./components/Experiences.jsx";
 import Hobbies from "./components/Hobbies.jsx";
 import Education from "./components/Education.jsx";
+import { Container, createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Lucida Console", "Courier New", monospace`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#678933",
+    },
+    secondary: {
+      main: "#333c89",
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <Header />
-      <div>
-        <Experiences />
-        <Hobbies />
-        <Education />
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth={{width: "100%"}}>
+        <Header />
+        <div>
+          WIP
+          {/*<Experiences />*/}
+          {/*<Hobbies />*/}
+          {/*<Education />*/}
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { AppBar, Avatar, Container, Stack, Typography } from "@mui/material";
+import photoUrl from '../assets/photo.jpeg'
+
+import './Header.scss'
 
 Header.propTypes = {};
 
 function Header(props) {
-  return <div>toto</div>;
+  return <AppBar className={'header-app-bar'} >
+    <Stack direction={"row"} gap={2}>
+      <Avatar src={photoUrl} sx={{width:64, height:64}}/>
+      <Stack>
+        <Typography variant={"h4"} component="div">Tomy MANSON</Typography>
+        <Typography variant={"subtitle2"} component="div">tomymanson@gmail.com</Typography></Stack>
+    </Stack>
+
+
+  </AppBar>;
 }
 
 export default Header;
