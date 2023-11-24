40156b5a363614314ceb7f2a8c43bf37f9a5941f
Tomy MANSON <mansoninformatique@gmail.com>
2023-11-24 20:58:56 +0100

    Passage sur tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
-import Experiences from "./components/Experiences.jsx";
-import Hobbies from "./components/Hobbies.jsx";
-import Education from "./components/Education.jsx";
-import { Container, createTheme, ThemeProvider } from "@mui/material";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: `"Lucida Console", "Courier New", monospace`,
-    fontSize: 14,
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-  },
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#678933",
-    },
-    secondary: {
-      main: "#333c89",
-    },
-  },
-});
-
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth={{width: "100%"}}>
+      <div className="flex p-0">
         <Header />
         <div>
           WIP
@@ -35,8 +10,7 @@ function App() {
           {/*<Hobbies />*/}
           {/*<Education />*/}
         </div>
-      </Container>
-    </ThemeProvider>
+      </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,36 @@
-import React from "react";
-import { AppBar, Avatar, Container, Stack, Typography } from "@mui/material";
-import photoUrl from '../assets/photo.jpeg'
+import photoUrl from "../assets/photo.jpeg";
 
-import './Header.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 Header.propTypes = {};
 
-function Header(props) {
-  return <AppBar className={'header-app-bar'} >
-    <Stack direction={"row"} gap={2}>
-      <Avatar src={photoUrl} sx={{width:64, height:64}}/>
-      <Stack>
-        <Typography variant={"h4"} component="div">Tomy MANSON</Typography>
-        <Typography variant={"subtitle2"} component="div">tomymanson@gmail.com</Typography></Stack>
-    </Stack>
-
-
-  </AppBar>;
+function Header() {
+  return (
+    <div className={"flex flex-1 items-end bg-gray-950 p-4 text-white"}>
+      <img className="flex-none h-24 w-24 rounded-full" src={photoUrl} alt="" />
+      <div className="flex flex-col pl-4 pb-3">
+        <div className="text-3xl font-bold">Tomy MANSON</div>
+        <div className="flex items-center text-gray-500 hover:text-gray-400 gap-1">
+          <FontAwesomeIcon icon={faAt} />
+          <a href="mailto:tomymanson@gmail.com" target="_blank" rel="noreferrer">
+            tomymanson@gmail.com
+          </a>
+        </div>
+        <div className="flex items-center text-gray-500 hover:text-gray-400 gap-1">
+          <FontAwesomeIcon icon={faLinkedin} />
+          <a
+            href="https://www.linkedin.com/in/tomy-manson/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://www.linkedin.com/in/tomy-manson/
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
